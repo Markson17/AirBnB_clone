15a46bf0122585401a3d3e7958fff22dac3fb7b2
Markson Favour <95889759+Markson17@users.noreply.github.com>
2023-02-07 23:57:18 +0000

    First Commit

---
+++ models/user.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+""" user module"""
+from models.base_model import BaseModel
+
+
+class User(BaseModel):
+    """ user class"""
+
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
+
+    def __init__(self, *arg, **kwargs):
+        """ defualt"""
+        super(User, self).__init__(*arg, **kwargs)
